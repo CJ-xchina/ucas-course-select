ae24fe542ebdb975ccf4b42ff9de44e1630aff60
cuijiexiang <1484895345@qq.com>
2024-06-04 12:47:11 +0800

    1

--- main.py
+++ main.py
@@ -14,100 +14,116 @@ import datetime
 from utils import driverOption, click, dataCollection, loop, ocrCal
 
 def select_course(config):
-    driver_path = config['driverPath']
-    options = webdriver.EdgeOptions()
-    driver = webdriver.Edge(executable_path=driver_path)
+    try:
+        options = webdriver.EdgeOptions()
+        driver = webdriver.Edge()
 
-    driver.get('https://sep.ucas.ac.cn/')
+        driver.get('https://sep.ucas.ac.cn/')
 
-    while True:
-        username_input = driver.find_element(By.ID, 'userName1')
-        password_input = driver.find_element(By.ID, 'pwd1')
-        username_input.send_keys(config['username'])
-        password_input.send_keys(config['password'])
-        if config.get('noCaptcha', False):
-            pass
-        else:
-            captcha_image = driver.find_element(By.ID, 'certCode1')
-            captcha_image.screenshot('captcha.png')
+        while True:
+            username_input = driver.find_element(By.ID, 'userName1')
+            password_input = driver.find_element(By.ID, 'pwd1')
+            username_input.send_keys(config['username'])
+            password_input.send_keys(config['password'])
+            if config.get('noCaptcha', False):
+                pass
+            else:
+                captcha_image = driver.find_element(By.ID, 'certCode1')
+                captcha_image.screenshot('captcha.png')
 
-            image = Image.open('captcha.png')
-            image = image.crop((100, 0, 250, 45))
-            image.save('captcha.png')
+                image = Image.open('captcha.png')
+                image = image.crop((100, 0, 250, 45))
+                image.save('captcha.png')
 
-            ocr = ddddocr.DdddOcr(beta=True)
-            with open('captcha.png', 'rb') as f:
-                image = f.read()
+                ocr = ddddocr.DdddOcr(beta=True)
+                with open('captcha.png', 'rb') as f:
+                    image = f.read()
 
-            res = ocr.classification(image)
-            print(res)
+                res = ocr.classification(image)
+                print(res)
 
-            captcha_image.send_keys(res)
+                captcha_image.send_keys(res)
 
-        login_button = driver.find_element(By.ID, 'sb1')
-        login_button.click()
+            login_button = driver.find_element(By.ID, 'sb1')
+            login_button.click()
 
-        wait = WebDriverWait(driver, 3)
+            wait = WebDriverWait(driver, 3)
 
-        try:
-            wait.until(EC.url_to_be('https://sep.ucas.ac.cn/appStore'))
-            break
-        except:
-            print("验证码输入错误，正在刷新页面...")
-            driver.refresh()
-
-    sleep(1)
-
-    click(driver, By.LINK_TEXT, '选课系统')
-    click(driver, By.LINK_TEXT, '选修课程', loop=False)
-    click(driver, By.LINK_TEXT, '院系课程选课')
-
-    sleep(2)
-
-    while True:
-        frame = driver.find_element(By.ID, config['subjectID'])
-        print(frame.is_selected())
-        frame.click()
-        print(frame.is_selected())
+            try:
+                wait.until(EC.url_to_be('https://sep.ucas.ac.cn/appStore'))
+                break
+            except:
+                print("验证码输入错误，正在刷新页面...")
+                driver.refresh()
 
         sleep(1)
 
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        click(driver, By.XPATH, '//button[contains(text(), "新增加本学期研究生课程")]')
-
-        sleep(1)
-
-        loop(driver, By.XPATH, config['courseID'])
-
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        ocrImage = driver.find_element(By.ID, 'adminValidateImg')
-        ocrImage.screenshot('ocrCal.png')
-
-        image = Image.open('ocrCal.png')
-        res = ocrCal('ocrCal.png', import_onnx_path='models/ocr_1.0_299_3000_2023-06-07-12-34-41.onnx', \
-                    charsets_path='models/charsets.json')
-
-        vcode = driver.find_element(By.ID, 'vcode')
-        vcode.send_keys(res)
-
-        submit_button = driver.find_element(By.XPATH, "//button[@name='sb' and @value='y']")
-        submit_button.click()
+        click(driver, By.LINK_TEXT, '选课系统')
+        click(driver, By.LINK_TEXT, '选修课程', loop=False)
 
         sleep(2)
 
-        current_url = driver.current_url
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        click(driver, By.XPATH, '//button[contains(text(), "保存联系方式")]')
 
-        confirm_button = driver.find_element(By.XPATH, "//button[text()='确定']")
-        confirm_button.click()
+        sleep(2)
+        click(driver, By.XPATH, '//button[contains(text(), "新增加本学期研究生课程")]')
 
-        print(f'[{datetime.datetime.now()}]选课完成！')
-        sleep(1)
+        sleep(2)
+        while True:
+            driver.refresh()
+            frame = driver.find_element(By.ID, config['subjectID'])
+            print(frame.is_selected())
+            frame.click()
+            print(frame.is_selected())
 
-        if driver.find_element(By.XPATH, \
-                    "//a[text()='"+config['courseID']+"']/ancestor::tr//button").is_enabled():
+            sleep(1)
+
+            loop(driver, By.XPATH, config['courseID'])
+
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            ocrImage = driver.find_element(By.ID, 'adminValidateImg')
+            ocrImage.screenshot('ocrCal.png')
+
+            image = Image.open('ocrCal.png')
+            res = ocrCal('ocrCal.png', import_onnx_path='models/ocr_1.0_299_3000_2023-06-07-12-34-41.onnx', \
+                        charsets_path='models/charsets.json')
+
+            vcode = driver.find_element(By.ID, 'vcode')
+            vcode.send_keys(res)
+
+            submit_button = driver.find_element(By.XPATH, "//button[@name='sb' and @value='y']")
+            submit_button.click()
+
+            sleep(2)
+
+            current_url = driver.current_url
+
+            confirm_button = driver.find_element(By.XPATH, "//button[text()='确定']")
+            confirm_button.click()
+
             print(f'[{datetime.datetime.now()}]选课完成！')
+            sleep(1)
+
+            if driver.find_element(By.XPATH, \
+                        "//a[text()='"+config['courseID']+"']/ancestor::tr//button").is_enabled():
+                print(f'[{datetime.datetime.now()}]选课完成！')
+                driver.quit()
+                return True
+    except Exception as e:
+        print(f'Error occurred: {e}')
+        driver.quit()
+        return False
+
+def start_thread(config):
+    while True:
+        success = select_course(config)
+        if success:
+            print(f"Thread for {config['courseID']} finished successfully.")
             break
+        else:
+            print(f"Thread for {config['courseID']} failed, restarting...")
 
 if __name__ == "__main__":
     with open('config.json', 'r') as f:
@@ -115,7 +131,7 @@ if __name__ == "__main__":
 
     threads = []
     for config in configs:
-        t = threading.Thread(target=select_course, args=(config,))
+        t = threading.Thread(target=start_thread, args=(config,))
         t.start()
         threads.append(t)
 
